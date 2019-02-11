60708ca3cdfabcf9f11474237662986610ea479f
Laura W <laura@isfluent.com>
2019-02-11 17:05:22 +0000

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
