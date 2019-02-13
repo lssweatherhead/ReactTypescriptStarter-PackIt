b9847e40af422c3137896978f71bc8c0ceafa631
Laura W <laura@isfluent.com>
2019-02-13 17:16:37 +0000

    Init styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css';
+import './App.scss';
 
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Hello, world</h1>
+      <React.Fragment>
+      <div className="section">
+        <div className="container">
+          <h1 className="title is-1"><i className="fas fa-map-signs"></i> Let's <span className="site-name">Pack It</span></h1>
+          <h2 className="subtitle is-family-secondary is-3">
+            Time to get away from it all
+          </h2>
+
+          
+        </div>
+        
       </div>
+      <div className="hero">
+        <div className="container">
+          <p>let's go</p>
+        </div>
+      </div>
+      </React.Fragment>
     );
   }
 }
