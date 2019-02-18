b671e5e8a00997509f1eca14db1829d2a324f072
Laura W <laura@isfluent.com>
2019-02-18 15:56:52 +0000

    Updated notes

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,20 @@ import Home from './pages/home';
 import Notes from './pages/notes';
 
 interface IState {
-  showNotes: boolean
+  showNotes: boolean,
+  activeStep?: number
 }
 
-class App extends Component<any, IState> {
-  constructor(props: any) {
+interface IProps {
+  currentStep: number
+}
+
+class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
     super(props);
     this.state = {
-      showNotes: true
+      showNotes: false,
+      activeStep: props.currentStep
     };
   }
 
@@ -21,26 +27,52 @@ class App extends Component<any, IState> {
     return (
       <React.Fragment>
         <div className="notes">
-          <a className="notes-link button is-small is-outlined is-link" href="" onClick={e => this.toggleNotes(e)}>
-            <span className="icon">
-              <i className="fas fa-eye"></i>
-            </span>
-            <span>{ this.state.showNotes ? "Show app" : "See notes"}</span>
-          </a>
+          <div className="buttons">
+            {this.props.currentStep > 0 &&
+              <a className="button is-text" href={"http://tutorials.lauraweatherhead.co.uk/react-typescript/step-" + (this.props.currentStep - 1)}>
+                <span className="icon">
+                  <i className="fas fa-arrow-left"></i>
+                </span>
+                <span>Previous step</span>
+              </a>
+            }
+
+            <a className="notes-link button is-outlined is-link" href="" onClick={this.toggleNotes}>
+              <span className="icon">
+                <i className="fas fa-eye"></i>
+              </span>
+              <span>{ this.state.showNotes ? "Show app [step " + this.props.currentStep + "]" : "See notes [step " + this.props.currentStep + "]"}</span>
+            </a>
+
+            {this.props.currentStep < 6 &&
+              <a className="button is-text" href={"http://tutorials.lauraweatherhead.co.uk/react-typescript/step-" + (this.props.currentStep + 1)}>
+                <span>Next step</span>
+                <span className="icon">
+                  <i className="fas fa-arrow-right"></i>
+                </span>
+              </a>
+            }
+          </div>
         </div>
         {this.state.showNotes ?
-          <Notes /> :
+          <Notes toggleState={this.toggleNotes} activeStep={this.state.activeStep} setActiveStep={this.toggleActiveStep} /> :
           <Home />
         }
-      </React.Fragment> 
+      </React.Fragment>     
     );
+  }
+
+  toggleActiveStep = (stepNumber?: number) => {
+    this.setState({
+      activeStep: stepNumber
+    });
   }
 
   toggleNotes = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     this.setState({
       showNotes: !this.state.showNotes
-    })
+    });
   }
 }
 
